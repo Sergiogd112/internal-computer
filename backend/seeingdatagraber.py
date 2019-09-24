a8c0e8fbc958505df3a610749a9eaefcddd7d391
Sergio <sergio.gomezdamas@gmail.com>
2019-09-24 18:33:20 +0200

    seeing parser

---
+++ backend/seeingdatagraber.py
@@ -0,0 +1,83 @@
+import numpy as np
+from pprint import pprint
+from collections import OrderedDict
+from bs4 import BeautifulSoup
+import mysql.connector
+import requests
+from requests_html import HTMLSession
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="teo",
+    passwd="01-01-2002"
+)
+
+
+print(mydb)
+
+url = "https://www.meteoblue.com/es/tiempo/outdoorsports/seeing/madrid_espa%c3%b1a_3117735"
+
+driver = webdriver.Firefox()
+driver.get(url)
+tablepath = '/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div/div[1]/table/tbody/tr[2]'
+tableroot = '/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div/div[1]/table/tbody'
+sleep(2)
+
+button = driver.find_element_by_xpath(
+    '/html/body/div[2]/div/div/div/div[2]/p[6]/button')
+print(button)
+button.click()
+sleep(5)
+table = driver.find_element_by_xpath(tablepath)
+print(table.text)
+sleep(4)
+i = 0
+while(True):
+    try:
+        table = driver.find_element_by_xpath(tableroot + 'tr[' + str(i) + ']')
+        print(table.text)
+    except:
+        break
+
+
+tbody = driver.find_element_by_xpath(tableroot)
+
+
+print('_' * 100)
+print('tbody')
+print(tbody.text)
+
+
+sleep(1)
+
+
+text = tbody.text
+celestial = tbody.find_elements_by_class_name('celestial')
+
+
+print('_' * 100)
+print([x for x in celestial])
+bodies = []
+# for x in celestial:
+#
+#     sleep(0.5)
+#     bodies.append(x.text)
+#     sleep(0.5)
+
+
+#driver.close()
+
+
+print(type(text))
+
+parsedrows = text.split('\n')
+parsedtable = [x.split() for x in parsedrows]
+
+
+for i in parsedtable:
+    if(type(i) == str):
+        pass
